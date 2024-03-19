309ea3149f1343832a3189b22fc44bd9fce9a804
AndreiKevin <81185469+AndreiKevin@users.noreply.github.com>
2024-03-19 22:55:46 +0800

    bat script

---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+
+start cmd /k "cd path_to_your_java_files && javac HydrogenClient.java && java HydrogenClient 5000"
+start cmd /k "cd path_to_your_java_files && javac OxygenClient.java && java OxygenClient 5000"
